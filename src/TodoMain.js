da5f330771676c27469c78c94b4347172a730efb
Isaac Lewis <isaacwlewis@gmail.com>
2020-02-04 07:05:31 -0600

    First draft of todo list app made with react

---
+++ src/TodoMain.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function TodoMain() {
+    return (
+        <div>
+                <input type = 'checkbox' /><p>Buy Banana</p>
+                <input type = 'checkbox' /><p>Water kids</p>
+                <input type = 'checkbox' /><p>Play Legos</p>
+                <input type = 'checkbox' /><p>Eat hot chip</p>
+        </div>
+    )
+}
+
+export default TodoMain
