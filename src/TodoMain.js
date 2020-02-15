dfd3362258cfe834e4a0fce0fdea9806d99046d1
Isaac Lewis <isaacwlewis@gmail.com>
2020-02-14 19:18:17 -0600

    made TodoMain a class based component in order to use state

--- src/TodoMain.js
+++ src/TodoMain.js
@@ -1,19 +1,30 @@
 import React from 'react'
 import TodoItem from './TodoItem'
 import todosDataq from './todosData'
+import todosData from './todosData'
 
-function TodoMain() {
-    const todoItemComponents = todosDataq.map(item => {
+class TodoMain extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            dataArray: todosData
+        }
+    }
+
+
+    render() {
+        const todoItemComponents = this.state.dataArray.map(item => {
             return (
                 <TodoItem item ={item} key = {item.id} />
             )
         })
 
-    return (
-        <div className = "list">
-            {todoItemComponents}
-        </div>
-    )
+        return (
+            <div className = "list">
+                {todoItemComponents}
+            </div>
+        )
+    }
 }
 
 export default TodoMain
